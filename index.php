678bcecfe0e10695d64c610ba3db79959ce3c96e
RonnyNavarro <59714406+RonnyNavarro@users.noreply.github.com>
2020-03-03 20:00:52 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?php
+    //Comentario 1 línea
+    /*Comentario
+    Multilineas*/
+    echo "<h1>Hola mundo PHP</h1>";
+    ?>
+
+</body>
+</html>
